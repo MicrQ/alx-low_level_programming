70396921f0b2cb454723db6f962ea791d1b61ce3
MicrQ <mail.micrq@proton.me>
2023-09-08 13:26:11 -0400

    task 100-print_comb3 done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 8)
+	{
+		int y = 1;
+
+		while (y <= 9)
+		{
+			if (x < y)
+			{
+				putchar(x + '0');
+				putchar(y + '0');
+				putchar(',');
+				putchar(' ');
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
