956b6618f82395acdef1e9f425788667d78d53ce
MicrQ <mail.micrq@proton.me>
2023-09-13 12:18:43 +0300

    task 1 is done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,18 @@
 #include "main.h"
 
+/**
+ * print_alphabet - prints alphabets in lowercase
+ *
+ * Return: Nothing
+ */
+void print_alphabet(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		_putchar(alp);
+		alp++;
+	}
+	_putchar('\n');
+}
