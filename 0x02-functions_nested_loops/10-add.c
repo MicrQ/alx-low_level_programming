40f8adf284b183cfbb41e5245b1771b5d07a608b
MicrQ <mail.micrq@proton.me>
2023-09-13 18:40:38 +0300

    task 10 is done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds its arguments
+ * Return: the added value will be returned
+ * @n: argument1
+ * @m: argument2
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
