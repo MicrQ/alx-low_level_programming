dab62f062e42f58839694ed60504a951ab60a612
MicrQ <mail.micrq@proton.me>
2023-09-13 22:01:21 +0300

    task 11 is edited

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,53 +1,49 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints numbers upto 98 from given number
- * @n: will be statrting point
+ * print_to_98 - prints numbers to 98
+ * print_number - prints num
  */
+void print_digit(int n)
+{
+	_putchar('0' + n);
+}
+void print_two_digits(int n)
+{
+	print_digit(n / 10);
+	print_digit(n % 10);
+}
+void print_three_digits(int n)
+{
+	print_digit(n / 100);
+	print_two_digits(n % 100);
+}
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if (n < 10)
+		print_digit(n);
+	else if (n < 100)
+		print_two_digits(n);
+	else
+		print_three_digits(n);
+}
 void print_to_98(int n)
 {
-	while (n <= 98)
+	while (n != 98)
 	{
-		int tmp, tmp1;
-
-		if (n < 0)
-		{
-			_putchar('-');
-			tmp = (-1 * n);
-			if (tmp < 10)
-				_putchar('0' + tmp);
-			else if(tmp < 100)
-			{
-				_putchar('0' + (tmp / 10));
-				_putchar('0' + (tmp % 10));
-			}
-			else
-			{
-				tmp1 = tmp % 10;
-				tmp = tmp / 10;
-				if (tmp > 10 && tmp < 100)
-				{
-					_putchar('0' + (tmp / 10));
-					_putchar('0' + (tmp % 10));
-					_putchar('0' + tmp1);
-				}
-			}
-		}
+		print_number(n);
+		_putchar(',');
+		_putchar(' ');
+		if (n > 98)
+			n--;
 		else
-		{
-			if (n >= 10)
-			{
-				_putchar('0' + (n / 10));
-				_putchar('0' + (n % 10));
-			}
-			else
-				_putchar('0' + n);
-		}
-		if(n != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		n++;
+			n++;
 	}
+	print_number(98);
+	_putchar('\n');
 }
