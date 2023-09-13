cfd59037560f7171bbc9e5226cc3685fb4b34306
MicrQ <mail.micrq@proton.me>
2023-09-13 22:17:21 +0300

    task 11 is modified

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,23 +1,35 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints numbers to 98
- * print_number - prints num
+ * print_digit - for 1 digit
+ * @n: will be calc
  */
 void print_digit(int n)
 {
 	_putchar('0' + n);
 }
+/**
+ * print_two_digits - for two digits
+ * @n: will be calc
+ */
 void print_two_digits(int n)
 {
 	print_digit(n / 10);
 	print_digit(n % 10);
 }
+/**
+ * print_three_digits -for three digits
+ * @n: will be checked
+ */
 void print_three_digits(int n)
 {
 	print_digit(n / 100);
 	print_two_digits(n % 100);
 }
+/**
+ * print_number - prints num
+ * @n: will be calc
+ */
 void print_number(int n)
 {
 	if (n < 0)
@@ -32,6 +44,10 @@ void print_number(int n)
 	else
 		print_three_digits(n);
 }
+/**
+ * print_to_98 - prints to 98 all
+ * @n: will be be calculated
+ */
 void print_to_98(int n)
 {
 	while (n != 98)
