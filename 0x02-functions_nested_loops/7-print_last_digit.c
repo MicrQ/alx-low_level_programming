633a6bab6e850693df733a08d5d8e4af6398d911
MicrQ <mail.micrq@proton.me>
2023-09-13 14:52:32 +0300

    task 7 is done

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @x: will be the number to be checked
+ * Return: returns the last number
+ */
+int print_last_digit(int x)
+{
+	return (x % 10);
+}
