baad5604a12843f614402dbedf79bf202656321a
MicrQ <mail.micrq@proton.me>
2023-09-13 17:07:59 +0300

    task 8 is done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - lists all possible hours from 00:00 upto 23:59
+ */
+void jack_bauer(void)
+{
+	int i = 0, j;
+
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar('0' + (i / 10));
+			_putchar('0' + (i % 10));
+			_putchar(':');
+                        _putchar('0' + (j / 10));
+                        _putchar('0' + (j % 10));
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
