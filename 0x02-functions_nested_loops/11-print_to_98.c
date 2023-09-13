a4b0fe1f79aaf44383dfa72742e4eb920d1baebc
MicrQ <mail.micrq@proton.me>
2023-09-13 21:13:30 +0300

    task 11 is modified

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,19 +8,46 @@ void print_to_98(int n)
 {
 	while (n <= 98)
 	{
+		int tmp, tmp1;
+
 		if (n < 0)
 		{
 			_putchar('-');
-			n *= -1;
-			if ((n / 10) != 0)
-				print_to_98(n / 10);
-			_putchar('0' + (n % 10));
+			tmp = (-1 * n);
+			if (tmp < 10)
+				_putchar('0' + tmp);
+			else if(tmp < 100)
+			{
+				_putchar('0' + (tmp / 10));
+				_putchar('0' + (tmp % 10));
+			}
+			else
+			{
+				tmp1 = tmp % 10;
+				tmp = tmp / 10;
+				if (tmp > 10 && tmp < 100)
+				{
+					_putchar('0' + (tmp / 10));
+					_putchar('0' + (tmp % 10));
+					_putchar('0' + tmp1);
+				}
+			}
 		}
 		else
 		{
-			if ((n / 10) != 0)
-				print_to_98(n / 10);
-			_putchar('0' + (n % 10));
+			if (n >= 10)
+			{
+				_putchar('0' + (n / 10));
+				_putchar('0' + (n % 10));
+			}
+			else
+				_putchar('0' + n);
 		}
+		if(n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		n++;
 	}
 }
