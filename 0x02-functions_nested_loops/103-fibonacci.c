3cc82beb63d32f5d25b4a5db259fe43bd723a679
MicrQ <mail.micrq@proton.me>
2023-09-14 03:09:18 +0300

    task 103 is done

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	long int a = 1, b = 2, c, sum = 0;
+
+	while (a <= 4000000)
+	{
+		if (a % 2 == 0)
+			sum += a;
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
