db87f5bfc10b998373f9dc3fdd8eec91350f6833
MicrQ <mail.micrq@proton.me>
2023-09-14 15:39:51 -0700

    task 3 is done

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,24 +12,25 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-    int day_of_year = 0;
-    int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int day_of_year = 0, tmp;
+	int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
-    {
-        days_in_month[1] = 29;
-    }
-    if (month < 1 || month > 12 || day < 1 || day > days_in_month[month - 1])
-    {
-        printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-        return;
-    }
-    for (int i = 0; i < month - 1; i++)
-    {
-        day_of_year += days_in_month[i];
-    }
-    day_of_year += day;
-    printf("Day of the year: %d\n", day_of_year);
-    printf("Remaining days: %d\n", (days_in_month[1] == 29 ? 366 : 365) - day_of_year);
+	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	{
+		days_in_month[1] = 29;
+	}
+	if (month < 1 || month > 12 || day < 1 || day > days_in_month[month - 1])
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+		return;
+	}
+	for (int i = 0; i < month - 1; i++)
+	{
+		day_of_year += days_in_month[i];
+	}
+	day_of_year += day;
+	printf("Day of the year: %d\n", day_of_year);
+	tmp = (days_in_month[1] == 29 ? 366 : 365) - day_of_year;
+	printf("Remaining days: %d\n", tmp);
 }
 
