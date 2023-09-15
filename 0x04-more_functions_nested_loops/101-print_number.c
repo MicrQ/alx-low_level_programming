5ba85d3ca10e050daa1b92b05a7e9b1ef422a876
micrq <mail.micrq@proton.me>
2023-09-15 15:33:59 +0300

    task 101 is modified

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,25 +1,37 @@
 #include "main.h"
 
 /**
- * print_number - prints given number
- * @n: will be printed only using putchar
+ * print_number - prints given numbers
+ * @n: will be printed
  */
 
-void print_number(int n)
+void  print_number(int n)
 {
-	int tmp;
-
+	int tmp, fact, digit, sign;
+/*
+*	if (n == 0)
+*		_putchar('0');
+*/
 	if (n < 0)
 	{
-		n = -n;
+		sign = -1;
 		_putchar('-');
 	}
-	if (n < 10)
-		_putchar('0' + n);
 	else
+		sign = 1;
+	tmp = n * sign;
+	fact = 1;
+	while (tmp > 9)
 	{
-		tmp = n % 10;
-		print_number(n / 10);
-		_putchar('0' + tmp);
+		tmp /= 10;
+		fact *= 10;
+	}
+	tmp = n * sign;
+	while (fact > 0)
+	{
+		digit = tmp / fact;
+		_putchar(digit + '0');
+		tmp %= fact;
+		fact /= 10;
 	}
 }
