e6ee268622ef172ce931afdb68d55b0586150815
micrq <mail.micrq@proton.me>
2023-09-15 14:47:06 +0300

    task 101 is done

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints given number
+ * @n: will be printed only using putchar
+ */
+
+void print_number(int n)
+{
+	int tmp;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (n < 10)
+		_putchar('0' + n);
+	else
+	{
+		tmp = n % 10;
+		print_number(n / 10);
+		_putchar('0' + tmp);
+	}
+}
