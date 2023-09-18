20bae14fd5a9b10e83c75c9665e1eaca6fe2e027
micrq <mail.micrq@proton.me>
2023-09-18 19:15:03 +0300

    task 9 is done

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string to another variable
+ * @dest: destination of string
+ * @src: source of string
+ * Return: the pointer to @dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, j;
+
+	while (*(src + i) != '\0')
+		i++;
+
+	j = 0;
+	while (j < i)
+	{
+		*(dest + j) = *(src + j);
+		j++;
+	}
+	return (dest);
+}
