d2d1e90a418d7efd914dddabac9a94f54323462d
micrq <mail.micrq@proton.me>
2023-09-20 22:47:57 +0300

    task 3 is done

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: 0 if the stirngs are equal, -ve value if s1 is less and +ve 
+ * value if s1 is greater
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, str1, str2, cmp;
+
+	while(s1[i] != '\0' && s2[i] != '\0')
+	{
+		str1 = s1[i];
+		str2 = s2[i];
+		if (str1 > str2)
+		{
+			cmp = str1 - str2;
+			break;
+		}
+			else if (str2 > str1)
+		{
+			cmp = str1 - str2;
+			break;
+		}
+		else
+		{
+			cmp = 0;
+			i++;
+		}
+	}
+	return (cmp);
+}
