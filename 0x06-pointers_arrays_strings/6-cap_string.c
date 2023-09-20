c89ed972af669c363061be17f1de0ee84fa76f92
micrq <mail.micrq@proton.me>
2023-09-21 01:52:32 +0300

    task 6 is done

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words in a string
+ * @c: a given string
+ * Return: modified string
+ */
+
+char *cap_string(char *c)
+{
+	int arr[] = " ,;.!	?\"(){}"
+	int i, j, size;
+
+	size = sizeof(c) / sizeof(int);
+	
+}
