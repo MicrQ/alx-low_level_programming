005565660425a2dbe754654544b12c8d03908c20
MicrQ <mail.micrq@proton.me>
2023-09-21 01:33:18 -0400

    task 7 modified

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,5 +8,24 @@
 
 char *leet(char *c)
 {
-  
+  int i = 0;
+  char ch;
+
+  while (c[i] != '\0')
+  {
+		ch = c[i];
+		if (ch == 'a' || ch == 'A')
+			c[i] = '0' + 4;
+		else if (ch == 'e' || ch == 'E')
+			c[i] = '0' + 3;
+		else if (ch == 'o' || ch == 'O')
+			c[i] = '0';
+		else if (ch == 't' || ch == 'T')
+			c[i] = '0' + 7;
+		else if (ch == 'l' || ch == 'L')
+			c[i] = '0' + 1;
+
+		i++;
+	}
+	return (c);
 }
