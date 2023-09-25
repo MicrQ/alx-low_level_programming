48f0a689d5599af3d24a6f590a2f16e32a3ee72e
micrq <mail.micrq@proton.me>
2023-09-25 17:37:13 +0300

    task 2 is done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+/**
+ * _strchr - locates a given character and returns a pointer to that character
+ * @s: given string
+ * @c: character to locate
+ * Return: s from the located character c
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	return ('\0');
+}
