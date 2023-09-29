40141e194dabdbf9121a5c051541f318ba05698a
MicrQ <mail.micrq@proton.me>
2023-09-29 08:41:03 -0400

    task 1 is modified

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * _print_rev_recursion - prints a given string in reverse
  * @s: given string
  */
@@ -9,7 +9,6 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 		return;
-	s++;
-	_print_rev_recursion(s);
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
