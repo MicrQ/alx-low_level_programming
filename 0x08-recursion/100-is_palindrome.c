3236ad90be8268e88f852df7a513f4db75f075c5
micrq <mail.micrq@proton.me>
2023-09-30 00:32:39 +0300

    task 100 is done

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,29 @@
+/**
+ * _print_rev_recursion - prints a given string in reverse
+ * @s: given string
+ * @sp: revesed string
+ */
+
+void _print_rev_recursion(char *s, char *sp)
+{
+	if (*s == '\0')
+		return;
+        _print_rev_recursion(s + 1, sp);
+        *sp = *s;
+	*sp++;
+}
+
+/**
+ * is_palindrome - checks is the string is palindrome or not
+ * @s: given string
+ * Return: 1 if the string is palindrome. else returns 0
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (_print_rev_recursion(s, ' '));
+}
+
+~
