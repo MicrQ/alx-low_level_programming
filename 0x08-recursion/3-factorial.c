e6bb964ded2891612dd17dd622f99b44a4d83e55
MicrQ <mail.micrq@proton.me>
2023-09-29 09:06:03 -0400

    task 3 is done

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+/**
+ * factorial - calculates the factorial of a given number
+ * @n: given number
+ * Return: the factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
