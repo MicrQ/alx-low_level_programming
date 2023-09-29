969323adca06affa86a9ce6c355b2b14ea2977b7
micrq <mail.micrq@proton.me>
2023-09-30 00:36:14 +0300

    task 100 is done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -23,7 +23,8 @@ int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-	return (_print_rev_recursion(s, ' '));
+	if (s == _print_rev_recursion(s, ' '))
+		return (1);
+	else
+		return (0);
 }
-
-~
