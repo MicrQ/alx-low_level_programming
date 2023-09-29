ad3d99f3dc88b03597595be5a830653ec332d0c4
micrq <mail.micrq@proton.me>
2023-09-30 01:23:38 +0300

    task 101 doneee

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,7 +10,7 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0')
 	{
 		if (*s2 != '\0' && *s2 == '*')
-			return (s1, s2 + 1);
+			return (wildcmp(s1, s2 + 1));
 		return (*s2 == '\0');
 	}
 	if (*s2 == '*')
