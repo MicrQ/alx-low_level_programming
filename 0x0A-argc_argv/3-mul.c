575ac257b8c49f89c7448585ae328906a87ecc12
MicrQ <mail.micrq@proton.me>
2023-10-02 07:49:56 -0400

    task 3 is done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry
+ * @argc: number of arguments
+ * @argv: vector arguments
+ * Return: 0 for success, 1 if argument is not passed
+ */
+
+int main(int argc, char* argv[])
+{
+	int n1, n2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	printf("%d\n", n1 * n2);
+	return (0);
+}
