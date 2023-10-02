4b774b30075a19100a797bd0b7122395a72a4947
MicrQ <mail.micrq@proton.me>
2023-10-02 09:16:25 -0400

    task 4 is done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+int _numcheck(char *s);
+
 /**
  * main - entry
  * @argc: number of arguments
@@ -11,18 +13,19 @@
 
 int main(int argc, char *argv[])
 {
-	int i, tmp, sum = 0;
+	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		tmp = atoi(argv[i]);
-		if (isdigit(tmp))
-			sum += tmp;
-		else
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
