d730ae6f022abaf224a99dcefb24ac741b70d331
MicrQ <mail.micrq@proton.me>
2023-10-06 10:13:02 -0400

    task 2 from project 0x0c is modified, memomry set to 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,12 +5,13 @@
  * @nmemb: number of array elements
  * @size: size of array elements
  * Return: a pointer to newly allocated memory or
- * 	   NULL if nmemb or size is 0 or the function fails.
+ *	   NULL if nmemb or size is 0 or the function fails.
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *arr;
+	char *arr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -18,5 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		arr[i] = 0;
 	return (arr);
 }
