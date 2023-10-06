bd47924d9bb62a5d433eaab5b68bf4f8dc2f225f
MicrQ <mail.micrq@proton.me>
2023-10-06 15:07:42 -0400

    task 4 from project 0x0c is modified

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 
 /**
- * _realloc - reallocates memory for a given pointer. 
+ * _realloc - reallocates memory for a given pointer.
  *		works as the C built-in realloc().
  * @ptr: void pointer to  reallocate memory
  * @old_size: old size of the pointer
  * @new_size: new size to reallocate
- * Return: 
+ * Return:
  *	# NULL - if function fails or new_size is 0 and ptr is not NULL
  *	# the new allocated memory
  *	# ptr - if old_size == new_size
@@ -14,7 +14,8 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *s;
+	char *s;
+	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
@@ -34,7 +35,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	s = malloc(new_size);
 	if (s == NULL)
 		return (NULL);
-	s = ptr;
+	for (i = 0; i < old_size; i++)
+		s[i] = ((char *)ptr)[i];
+	free(ptr);
 
 	return (s);
 }
