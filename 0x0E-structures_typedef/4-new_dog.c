f54660f2b4394416520e1d2b92ab09dc196c5fef
MicrQ <mail.micrq@proton.me>
2023-10-09 12:42:21 -0400

    task 4 from project 0x0e is modified by copying name and owner using _strcpy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,45 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - returns the length of the given string
+ * @s: is the string passed
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	while (*(s + i) != '\0')
+		i++;
+	return (i);
+}
+
+
+/**
+ * _strcpy - copies string to another variable
+ * @dest: destination of string
+ * @src: source of string
+ * Return: the pointer to @dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, j;
+
+	while (*(src + i) != '\0')
+		i++;
+
+	j = 0;
+	while (j <= i)
+	{
+		*(dest + j) = *(src + j);
+		j++;
+	}
+	return (dest);
+}
+
+/**
  * new_dog - creates new dog
  * @name: name of the new dog
  * @age: of the new dog
@@ -12,12 +51,34 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dg;
+	char *tn, *to;
+	unsigned int nl, ol;
+	
 
 	dg = malloc(sizeof(dog_t));
 	if (dg == NULL)
 		return (NULL);
-	dg->name = name;
-	dg->owner = owner;
+	for (nl = 0; name[nl] != '\0'; nl++)
+	for (ol = 0; owner[ol] != '\0'; ol++)
+
+	tn = malloc(sizeof(char) * (nl + 1));
+	if (tn == NULL)
+	{
+		free(dg);
+		return (NULL);
+	}
+	to = malloc(sizeof(char) * (ol + 1));
+	if (to == NULL)
+	{
+		free(dg);
+		free(tn);
+		return (NULL);
+	}
+	tn = name;
+	to = owner;
+
+	dg->name = tn;
+	dg->owner = to;
 	dg->age = age;
 	return (dg);
 }
