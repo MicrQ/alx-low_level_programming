b19391213eb0830516d261802d48fd3cba42515e
MicrQ <mail.micrq@proton.me>
2023-10-09 11:24:33 -0400

    task 1 from project 0x0e is done

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - initializes the variable elements of type struct dog
+ * @d: a pointer to the variable of type d
+ * @name: of the variable d
+ * @age: of the variable d
+ * @owner: of the variable d
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
