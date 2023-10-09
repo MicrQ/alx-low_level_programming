11961d41b5e11e77c903116749e9bbfeeed40a0e
MicrQ <mail.micrq@proton.me>
2023-10-09 10:55:56 -0400

    comment for dog structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef MY_HEADER_H
 #define MY_HEADER_H
 
+/**
+ * struct dog - my first structure project
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: of the dog
+ * Description: the dog structure has 3 elements
+ */
 struct dog
 {
 	char *name;
