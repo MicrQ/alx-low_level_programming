05f1cb2117d9ccd49a83076f3ddf1a7172720bb7
MicrQ <mail.micrq@proton.me>
2023-10-11 07:33:23 -0400

    task 0 from project 0x0f modified again

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
  * print_name - prints the value of name passed to it using the passed
  *		function pointers
@@ -7,5 +9,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	(*f)(name);
 }
