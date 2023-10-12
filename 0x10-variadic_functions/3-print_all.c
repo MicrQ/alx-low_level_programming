c0459415140f9fe5dd43eee0141f97420dc6843c
MicrQ <mail.micrq@proton.me>
2023-10-12 09:20:14 -0400

    task 3 from project 0x10 is done

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * print_all - prints anything passed to it
+ * @format: the variable arguments format
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i = 0, flag;
+	char *str;
+	va_list ags;
+
+	va_start(ags, format);
+	while (format[i] != '\0')
+	{
+		flag = 1;
+		switch (format[i])
+		{
+			case 'i':
+				printf("%d", va_arg(ags, int));
+				break;
+			case 'c':
+				printf("%c", va_arg(ags, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ags, double));
+				break;
+			case 's':
+				str = va_arg(ags, char *);
+				if (str == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+			default:
+				flag = 0;
+		}
+		if (format[i + 1] != '\0' && flag == 1)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(ags);
+}
