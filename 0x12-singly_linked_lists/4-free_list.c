e2745321c77cccf0637fbcd826420d8f6fbc179f
MicrQ <mail.micrq@proton.me>
2023-10-20 19:58:14 -0400

    task 4 from project 0x12 is done

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - helps to free a memory of type list_t(aka struct list_s)
+ * @head: a pointer to the first element
+ */
+
+void free_list(list_t *head)
+{
+	list_t *hold;
+
+	while(head != NULL)
+	{
+		hold = head->next;
+		free(head->str);
+		free(head);
+		head = hold;
+	}
+}
