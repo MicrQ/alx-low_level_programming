7a6298e597a31e187208107164771e7f4f77edb2
MicrQ <mail.micrq@proton.me>
2023-10-23 13:53:17 -0700

    task 2 of project 0x13 is modified

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,7 +9,7 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *ptr = malloc(sizeof(int));
+	listint_t *ptr = malloc(sizeof(listint_t));
 
 	ptr->n = n;
 	ptr->next = (*head);
