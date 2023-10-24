e60e3b96c9b5726e7dafdc93d88fe5a8e94d469b
MicrQ <mail.micrq@proton.me>
2023-10-24 05:48:01 -0700

    task 9 from project 0x13 is done

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * listint_len - is a function that computes the number of elements
+ * @h: a pointer to struct listint_s type
+ * Return: the number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
+
+/**
+ * insert_nodeint_at_index - inserts new node at a given index
+ * @head: a pointer to a linked list
+ * @idx: the index of the new node
+ * @n: data for the new node
+ * Return: the address of the new node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *ptr, *Head = *head;
+	unsigned int i;
+
+	if (head == NULL || Head == NULL)
+		return (NULL);
+
+	if (idx >= listint_len(Head))
+		return (NULL);
+
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->n = n;
+
+	for (i = 0; i < idx; i++)
+		Head = Head->next;
+	ptr->next = Head->next;
+	Head->next = ptr;
+
+	return (ptr);
+}
