f89d82fe809a1f3759d4a38e3a519d641b230910
MicrQ <mail.micrq@proton.me>
2023-10-24 07:19:25 -0700

    task 10 of project 0x13 is done

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint - deletes a specified node of the list
+ * @head: a pointer to the linked list
+ * @index: the index of the node to be deleted
+ * Return:  1 for success and -1 for failure
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *ptr = NULL, *Head = *head;
+	unsigned int i;
+
+	if (*head == NULL)
+		return (-1);
+	
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(Head);
+		return (1);
+	}
+
+	for (i = 0; i < index; i++)
+	{
+		if (Head == NULL || Head->next == NULL)
+			return (-1);
+		if (i == index - 1)
+		{
+			ptr = Head->next->next;
+			free(Head->next);
+			Head->next = ptr;
+			return (1);
+		}
+		Head = Head->next;
+	}
+	return (-1);
+}
