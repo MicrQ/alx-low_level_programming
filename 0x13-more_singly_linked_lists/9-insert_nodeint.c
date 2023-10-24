b701ea4c824af8c1143a9cb56a0261972ab12456
MicrQ <mail.micrq@proton.me>
2023-10-24 05:53:01 -0700

    file 9-insert_nodeint.c modified

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ptr, *Head = *head;
 	unsigned int i;
 
-	if (head == NULL || Head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	if (idx >= listint_len(Head))
