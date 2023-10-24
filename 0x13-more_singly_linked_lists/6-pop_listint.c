9bc0d1a54269ebc0e8ae7979c998b425999a8982
MicrQ <mail.micrq@proton.me>
2023-10-24 04:14:16 -0700

    task 6 from project 0x13 is done

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head element
+ * @head: a pointer to the linked list
+ * Return: the deleted node data or 0 if the head is empty
+ */
+
+int pop_listint(listint_t **head)
+{
+	int tmp = (*head)->n;
+	listint_t *ptr = (*head)->next;
+
+	if (head == NULL)
+		return (0);
+
+	free(*head);
+	*head = ptr;
+	return (tmp);
+}
