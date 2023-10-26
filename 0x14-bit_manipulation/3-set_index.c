5464e649139f2248eb6d9aead9333dfc016042e5
MicrQ <mail.micrq@proton.me>
2023-10-27 00:28:41 +0300

    task 3 from project 0x14 is done

---
+++ 0x14-bit_manipulation/3-set_index.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - sets a given index place of number to 1
+ * @n: a pointer to the number
+ * @index: position of bit
+ * Return: 1 if succeed -1 if failed
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int lsh;
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	lsh = 1 << index;
+	*n = *n | lsh;
+	return (1);
+}
