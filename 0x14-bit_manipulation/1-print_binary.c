84b210de225ea64414374a609305e7fa59790538
MicrQ <mail.micrq@proton.me>
2023-10-26 20:26:01 +0300

    task 1 is modified for case 0

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,15 +1,30 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_binary - converts given unsigned int to binary and prints the binary
+ * print_binary_rev - converts given unsigned int to binary and prints the binary
  * @n: number to be converted
+ */
+
+void print_binary_rec(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	print_binary_rec(n >> 1);
+
+	_putchar('0' + (n & 1));
+}
+
+/**
+ * print_binary - a function that calls print_binary_rev
+ * @n: given number
  */
 
 void print_binary(unsigned long int n)
 {
 	if (n == 0)
+	{
+		_putchar('0');
 		return;
-	print_binary(n >> 1);
-
-	_putchar('0' + (n & 1));
+	}
+	print_binary_rec(n);
 }
