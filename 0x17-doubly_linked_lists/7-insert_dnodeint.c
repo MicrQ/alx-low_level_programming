e1414f640cdc287fc572f4c3590519c09d8cf460
MicrQ <mail.micrq@proton.me>
2023-12-07 23:12:21 +0300

    Modified-E: a function that inserts a new node at a given indext(position)

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,83 +1,63 @@
 #include "lists.h"
 /**
- * free_node - a function that frees a single node
- * @node: a given node
+ * _len - node counter
+ * @head: ptr
+ * Return: number of node
+ */
+size_t _len(dlistint_t *head)
+{
+	size_t i = 0;
+
+	while (head)
+	{
+		i++;
+		head = head->next;
+	}
+	return (i);
+}
+
+/**
+ * insert_dnodeint_at_index - a function that inserts a node at idx place
+ * @h: a pointer to the dlist
+ * @idx: an index to insert a node
+ * @n: data for the new node
+ *
  * Return: NULL
  */
-dlistint_t *free_node(dlistint_t *node)
-{
-	free(node);
-	return (NULL);
-}
-
-/**
- * _len - a function that counts number of nodes
- *
- * @h: a head pointer to the linked list
- * Return: number of nodes
- */
-size_t _len(const dlistint_t *h)
-{
-	size_t nodes = 0;
-
-	while (h)
-	{
-		nodes++;
-		h = h->next;
-	}
-	return (nodes);
-}
-
-/**
- * insert_dnodeint_at_index - a function that adds a new node at a given index
- *
- * @h: a pointer to the list
- * @idx: the position where the new node should be placed
- * @n: the data to be added in the new node
- *
- * Return: the new node or NULL on failure
- */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *head = *h, *new = malloc(sizeof(dlistint_t));
-	unsigned int len = _len(*h), i = 0;
+	size_t i = 0;
 
 	if (!new)
 		return (NULL);
-	if (idx >= len)
-		return (free_node(new));
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
-	if (!*h)
+
+	if (idx == 0)
 	{
-		if (idx == 0)
+		if (*h)
 		{
-			*h = new;
-			return (new);
+			new->next = *h;
+			(*h)->prev = new;
 		}
-	}
-	else if (idx == 0)
-	{
-		new->next = *h;
-		(*h)->prev = new;
 		*h = new;
 		return (new);
 	}
-	else
-		while (head)
+	while (head)
+	{
+		if (idx == i + 1 && idx < _len(*h))
 		{
-			if (i == idx)
-			{
-				new->next = head;
-				new->prev = head->prev;
-				head->prev->next = new;
-				head->prev = new;
-				return (new);
-			}
-			head = head->next;
-			i++;
+			new->next = head->next;
+			head->next->prev = new;
+			new->prev = head;
+			head->next = new;
+			return (new);
 		}
-	return (free_node(new));
+		head = head->next;
+		i++;
+	}
+	free(new);
+	return (NULL);
 }
