e7d8fab4d0cf4e40510f047d79438cfb9069c9a7
MicrQ <mail.micrq@proton.me>
2023-12-07 13:31:48 +0300

    a function that counts number of nodes in doubly linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - a function that counts number of nodes
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
