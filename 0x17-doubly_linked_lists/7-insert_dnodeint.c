bf7294851e8afa248209b608aa23cbdc98ca9a62
MicrQ <mail.micrq@proton.me>
2023-12-07 21:17:25 +0300

    Modified-C: a function that inserts a new node at a given indext(position)

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *head = *h, *new = malloc(sizeof(dlistint_t));
 	unsigned int len = _len(*h), i = 0;
 
-	if (!new || idx >= len)
+	if (!new || idx > len)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
