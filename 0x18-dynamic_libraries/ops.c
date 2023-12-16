6669c8a8cd84a23a97ab82444c6d44b748c76a7e
MicrQ <mail.micrq@proton.me>
2023-12-16 10:57:15 +0300

    a c functions library to be used in python

---
+++ 0x18-dynamic_libraries/ops.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+/**
+ * add - adds
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subs
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - muls
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divs
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mods
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
